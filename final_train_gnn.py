08980c1ba077752d229883d29ceeee23788504c6
Mahim Haque <mahimanzum@gmail.com>
2023-04-19 23:18:53 -0400

    comments added and code cleaned

--- final_train_gnn.py
+++ final_train_gnn.py
@@ -11,20 +11,21 @@ import math
 from torch_geometric.nn import GCNConv, GATv2Conv
 from torch_geometric.loader import DataLoader
 import copy
+from tqdm import tqdm
 torch.manual_seed(12345)
 
 
+### distance matrix calculation for using in the graph
 
 positions = open('labapp3-positions.txt', 'r').read().strip()
 positions = positions.split("\n")
 positions = [x.split(" ") for x in positions]
-#print(positions)
 positions = [[float(x) for x in data] for data in positions]
 position = {}
 for dt in positions:
     position[int(dt[0])] = (dt[1], dt[2])
 
-from tqdm import tqdm
+# reading the main data file for processing
 data_file = "labapp3-data-new.txt"
 f = open(data_file, "r").read()
 data = f.split("\n")
@@ -36,8 +37,21 @@ for i in tqdm(data):
             X.pop()
     except:
         pass
+
+# MAX MIN array calculation for each column for normalizing the data later
+
 MIN_S = np.min(np.array(X), axis=0)
 MAX_S = np.max(np.array(X), axis=0)
+
+
+#creating a dictionary of data for each time stamp
+'''
+Dictionary structure:
+{time_stamp: {sensor_id_1: [sensor_data],
+             sensor_id_2: [sensor_data], ...},
+time_stamp_2: {sensor_id_1: [sensor_data], 
+            sensor_id_2: [sensor_data], ...}, ...
+'''
 num_sensors = 54
 from collections import defaultdict
 data_dict = defaultdict(dict)
@@ -48,31 +62,39 @@ for dt in tqdm(X):
         data_dict[int(dt[0])][int(dt[1])] = dt[2:]
 
 
-
+# loop for creating the graph data for each time stamp
 graphs = []
+
+# change the line below to change the number of time stamps to be used for training
 for time in tqdm(sorted(list(data_dict.keys()))[:100]):
     #print(time)
     sensor_data = np.zeros((num_sensors, 4))
     for sensor_id in data_dict[time]:
         try:
+            # converting one indexing to zero indexing
             sensor_data[sensor_id-1] = (np.array(data_dict[time][sensor_id])-MIN_S[2:])/(MAX_S[2:]-MIN_S[2:])
         except:
-            #print(np.array(data_dict[time][sensor_id]))
-            #print(MIN_S[2:])
             continue
     
+    # creating the adjacency matrix
     adj_matrix = np.ones((num_sensors, num_sensors))
 
+    #list of indices of sensors which are not present in the current time stamp
     a = [i for i in range(num_sensors) if i not in (np.array(list(data_dict[time].keys()))-1).tolist() ]
 
+    # removing the edges from the adjacency matrix for those absent sensors
     for idx in a:
         for i in range(num_sensors):
             adj_matrix[idx][i] = 0
             adj_matrix[i][idx] = 0
 
+    # removing self connections from the adjacency matrix because we want to predict the sensor data from other sensors
     np.fill_diagonal(adj_matrix, 0)
+    # creating the edge list from the adjacency matrix with pyg graph edge list format
     temp = np.transpose(np.nonzero(adj_matrix)).reshape(1, -1)
     edge_list = np.array([np.array(temp[0][::2]) , np.array(temp[0][1::2])])
+
+    # calculating the edge attributes for the graph
     edge_attr = []
     for idx in range(edge_list.shape[1]):
         fm, to = edge_list[0][idx]+1, edge_list[1][idx]+1
@@ -83,29 +105,26 @@ for time in tqdm(sorted(list(data_dict.keys()))[:100]):
              edge_index=torch.tensor(edge_list,dtype=torch.long), 
              y=torch.tensor(sensor_data, dtype=torch.float),
              edge_attr=torch.tensor(edge_attr.reshape(-1, 1), dtype=torch.float))
-             
     
-    #g = Data(x=torch.rand((num_cols, num_cols), dtype=torch.float), edge_index=torch.tensor(edge_list,dtype=torch.long))
-    
-    #g.y = torch.tensor(np.array(sensor).reshape(-1, 1), dtype=torch.float)
-    
+    # creating the train mask so that the loss is only calculated for the present sensors
     g.train_mask = np.array([False]*num_sensors)
     g.train_mask[np.array(list(data_dict[time].keys()))-1] = True
     g.train_mask = torch.tensor(g.train_mask)
-    #print("train mask shape", g.train_mask.shape)
-    #print("input shape",g.x.shape)
+ 
 
+    # creating the test mask so that the loss is not calculated for the absent sensors
     g.test_mask = np.array(([True]*num_sensors))
     g.test_mask[np.array(list(data_dict[time].keys()))-1] = False
     g.test_mask = torch.tensor(g.test_mask)
 
-    
     graphs.append(g)
 
 print(len(graphs))
 #graphs = graphs[:10000]
 
 loader = DataLoader(graphs, batch_size=10, shuffle=False)
+
+# three models are used for comparison
 
 class GCN(torch.nn.Module):
     def __init__(self):
@@ -212,8 +231,7 @@ def train_node_classifier(model, graphs, optimizer, criterion, n_epochs=100):
 
     return model
 
-#squeeze = lambda x: x.squeeze(1)
-
+# redundant code (can be removed but kept for reference)
 adj_matrix = np.ones((num_sensors, num_sensors))
 np.fill_diagonal(adj_matrix, 0)
 temp = np.transpose(np.nonzero(adj_matrix)).reshape(1, -1)
@@ -230,18 +248,20 @@ def eval_node_classifier(model, graphs):
     model.eval()
     with torch.no_grad():
         for idx, graph in tqdm(enumerate(graphs)):
+            g = copy.deepcopy(graph)    
+            g.edge_index = torch.tensor(edge_list,dtype=torch.long)
+            g.edge_attr = torch.tensor(edge_attr.reshape(-1, 1), dtype=torch.float)
+            out = model(g)
+            out_list = out.detach().numpy().tolist()
+
             for i in range(num_sensors):
-                print(idx, graph.train_mask)
+                # we want a prediction for each sensor
                 if not graph.train_mask[i]:
-                    g = copy.deepcopy(graph)    
-                    g.edge_index = torch.tensor(edge_list,dtype=torch.long)
-                    g.edge_attr = torch.tensor(edge_attr.reshape(-1, 1), dtype=torch.float)
-                    out = model(g)
-                    out_list = out.detach().numpy().tolist()
-                    out_final.append([(i+1)*30, i+1]+out_list[i])
+                    # if this sensor is not in the training set, we want to predict and use the imputed value
+                    out_final.append([(idx+1)*30, i+1]+out_list[i])
                 else:
-                    print(data_dict[(i+1)*30])
-                    out_final.append([(i+1)*30, i+1]+data_dict[(i+1)*30][i+1])
+                    # if this sensor is in the training set, we want to use the ground truth value
+                    out_final.append([(idx+1)*30, i+1]+((np.array(data_dict[(idx+1)*30][i+1])-MIN_S[2:])/(MAX_S[2:]-MIN_S[2:])).tolist())
 
 
 #model = GCN().to('cpu')
@@ -264,3 +284,13 @@ eval_node_classifier(model, graphs)
 # save out_final to csv with column names
 mod_df = pd.DataFrame(out_final, columns=["time", "nodeid", "temperature" ,"humidity", "light", "voltage"])
 mod_df.to_csv('modified_env.csv', index=False)
+
+'''
+30,1,0.20009765028953552,0.0059858085587620735,0.7404278516769409,-0.0762353166937828
+30,2,0.2406468242406845,0.01273853424936533,0.838738203048706,-0.046470265835523605
+30,3,0.1382274133896898,0.988967344623328,0.027390438247011956,0.8541260865461349
+30,4,0.2236645221710205,-0.018838858231902122,0.7683352828025818,-0.02886117435991764
+30,5,0.214375302195549,-0.050112172961235046,0.812414824962616,-0.05435218662023544
+30,6,0.1387359423352706,0.9889371603446336,0.06573705179282868,0.8388215635376892
+30,7,0.2642294466495514,-0.007743816822767258,0.8069591522216797,-0.06117755547165871
+'''
